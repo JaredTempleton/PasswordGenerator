f6862a6744b24d626fa95cc477ae58892c4a5950
JaredTempleton <95387966+JaredTempleton@users.noreply.github.com>
2022-01-16 19:05:58 -0500

    Update script.js
    added password function

--- Develop/script.js
+++ Develop/script.js
@@ -6,13 +6,56 @@ var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var special = ["!", "?", "%", "&", ",", "*", "+", "-", "."];
 
-// Prompts
+// Declare varables
 var lengthConfirm = "";
 var lowerConfirm;
 var upperConfirm;
 var numberConfirm;
 var specialConfirm;
 
+// Password generation functions
+function generatePassword() {
+  // Character amount confirmation
+  var lengthConfirm = (prompt("Please let us know how many characters you would like your password to contain (between 8-128)"));
+    while(lengthConfirm <=7 || lengthConfirm >=129) {
+    alert("Password length MUST be between 8-128 characters")
+    var lengthConfirm = (prompt("Please let us know how many characters you would like your password to contain (between 8-128)"));
+  }
+  // Character Confirmations
+  var lowerConfirm = confirm("Click OK if you would like your password to contain lowercase letters, otherwise click cancel");
+  var upperConfirm = confirm("Click OK if you would like your password to contain uppercase letters, otherwise click cancel");
+  var numberConfirm = confirm("Click OK if you would like your password to contain numbers, otherwise click cancel")
+  var specialConfirm = confirm("Click OK if you would like your password to contain special characters, otherwise click cancel")
+  // while loop to make sure the user selects at least one parameter
+    while( lowerConfirm === false && upperConfirm === false && numberConfirm === false && specialConfirm === false) {
+      alert("You MUST select at least one paramenter for your password");
+      var lowerConfirm = confirm("Click OK if you would like your password to contain lowercase letters, otherwise click cancel");
+      var upperConfirm = confirm("Click OK if you would like your password to contain uppercase letters, otherwise click cancel");
+      var numberConfirm = confirm("Click OK if you would like your password to contain numbers, otherwise click cancel")
+      var specialConfirm = confirm("Click OK if you would like your password to contain special characters, otherwise click cancel")
+    }
+  var passwordCharacters = []
+  if (lowerConfirm) {
+    passwordCharacters = passwordCharacters.concat(lowerCase)
+  }
+  if (upperConfirm) {
+    passwordCharacters = passwordCharacters.concat(upperCase)
+  }
+  if (numberConfirm) {
+    passwordCharacters = passwordCharacters.concat(number)
+  }
+  if (specialConfirm) {
+    passwordCharacters = passwordCharacters.concat(special)
+  }
+  console.log(passwordCharacters)
+
+  var passwordRandomizer = ""
+  for (var i = 0; i < lengthConfirm; i++) {
+    passwordRandomzier = passwordRandomizer + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+    console.log(passwordRandomizer)
+  }
+  return passwordRandomizer
+  }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
